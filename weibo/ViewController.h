b58ebd1394b57fbe5bf35adbc71a063988895dc9
Halt <454752040@qq.com>
2015-08-03 11:25:57 +0800

    Initial Commit

---
+++ weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by Colin on 15/8/3.
+//  Copyright (c) 2015年 wjj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
